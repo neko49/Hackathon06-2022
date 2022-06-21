2465aa147998db4264d80717e881bdae88402a19
neko49 <noudehouen-regis-jaures.fassinou@estiam.com>
2022-06-21 16:40:03 +0200

    Commit

--- frontend/src/components/Projects/ProjectList.jsx
+++ frontend/src/components/Projects/ProjectList.jsx
@@ -1,18 +1,20 @@
 import React, {Fragment} from 'react';
+import { MDBDataTableV5 } from 'mdbreact';
 import './ProjectList.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import defaultTable from '../../data/defaultTable';
 
-const defaultTable = import('../../data/defaultTable')
+
 
 const ProjectList = () => {
 
     const [datatable, setDatatable] = React.useState(defaultTable);
-
+    console.log(defaultTable)
     return (
         <Fragment>
-            <p>Test</p>
+            <MDBDataTableV5 hover entriesOptions={[5, 20, 25]} entries={5} pagesAmount={4} data={defaultTable} fullPagination />;
         </Fragment>
     )
 }
