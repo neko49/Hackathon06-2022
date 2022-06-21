3680df68ec0ef5c2c7888e1069fa272e76a73c1e
neko49 <noudehouen-regis-jaures.fassinou@estiam.com>
2022-06-21 14:32:06 +0200

    Commit ProjetList

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home/home';
-import ProjetList from './projetList/ProjetList';
+import ProjetList from './components/projetList/ProjetList';
+
 
 function App() {
   return (
