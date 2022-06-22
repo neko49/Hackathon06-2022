80dae71d1795fa0ae8d0dd6a04340154143863f6
lmoriceau <lmoriceau@mediamatis.com>
2022-06-22 15:55:37 +0200

    Updated backend

--- backend/routes/project.route.js
+++ backend/routes/project.route.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const cors = require("cors");
-const {getAll, getJobs, getEnvVars} = require("../controllers/project.controller");
+const {getAll, getJobs, getEnvVars, getApps, getPipelines} = require("../controllers/project.controller");
 
 
 router.use(cors());
@@ -13,6 +13,12 @@ router.get('/:id/jobs', async (req, res) => {
 });
 router.get('/:id/envvars', async (req, res) => {
   getEnvVars(req, res)
+});
+router.get('/:id/apps', async (req, res) => {
+  getApps(req, res)
+});
+router.get('/:id/pipelines', async (req, res) => {
+  getPipelines(req, res)
 });
 
 
