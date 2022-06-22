d2e575b520133d3c6113a3157f8a90bc10f99fe9
lmoriceau <lmoriceau@mediamatis.com>
2022-06-22 16:08:12 +0200

    Updated backend

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -3,6 +3,9 @@ const projectService = require('../services/project.service');
 const envVarService = require('../services/envVar.service');
 const appService = require('../services/app.service');
 const pipelineService = require('../services/pipeline.service');
+const JSZip = require('jszip');
+const fs = require("fs");
+const zip = new JSZip();
 
 function getAll(req, res, next) {
     const jwt = req.headers?.authorization
@@ -88,10 +91,52 @@ function getPipelines(req, res) {
     });
 }
 
+
+async function backup(req, res) {
+    const jwt = req.headers?.authorization
+    const jobs = await envVarService.getAll(jwt, req.params.id).then(jobs => {
+        return jobs.data
+    }).catch((fail) => {
+        console.log(fail)
+        return null;
+    });
+    const envVars = await envVarService.getAll(jwt, req.params.id).then(envVars => {
+        return envVars.data
+    }).catch((fail) => {
+        console.log(fail)
+        return null;
+    });
+    const apps = await appService.getAll(jwt, req.params.id).then(apps => {
+        return apps.data
+    }).catch((fail) => {
+        console.log(fail)
+        return null;
+    });
+    const pipelines = await pipelineService.getAll(jwt, req.params.id).then(pipelines => {
+        return pipelines.data
+    }).catch((fail) => {
+        console.log(fail)
+        return null;
+    });
+    const data = {
+        jobs: jobs,
+        envVars: envVars,
+        apps: apps,
+        pipelines: pipelines
+    };
+    const compressed = zip.file("data.json", JSON.stringify(data));
+    zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true })
+        .pipe(fs.createWriteStream("data.zip"));
+    res.setHeader('Content-Length', compressed.length);
+    res.write(compressed, 'binary');
+    res.end();
+}
+
 module.exports = {
     getAll,
     getJobs,
     getEnvVars,
     getApps,
-    getPipelines
+    getPipelines,
+    backup
 }
