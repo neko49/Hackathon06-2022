9cfa2b7a5cab3783ed3883ac2156cec9f3b71693
neko49 <noudehouen-regis-jaures.fassinou@estiam.com>
2022-06-22 11:08:40 +0200

    pull

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+<<<<<<< Updated upstream
+=======
+import "./App.css";
+import ProjectList from "./components/Projects/ProjectList";
+import Log from "./components/Login/login";
+
+function App() {
+  return 1 == 0 ? <ProjectList /> : <Log />;
+}
+
+export default App;
+>>>>>>> Stashed changes
